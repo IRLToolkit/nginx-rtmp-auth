610324d8f428c1bee13dd33bfc960ea9a61ab1b4
tt2468 <tt2468@gmail.com>
2020-04-21 06:14:38 -0700

    Convert everything to async and do a bunch of other stuff

--- rtmpauth.py
+++ rtmpauth.py
@@ -1,82 +1,41 @@
-# COPYRIGHT (C) 2018 TT2468 (https://github.com/tt2468) DO NOT USE OR DISTRIBUTE WITHOUT EXPRESSED PERMISSION
-
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-import sys
 import logging
+import aiohttp
+import sys
 import json
-from configparser import SafeConfigParser
+from configparser import ConfigParser
 
-rtmpconfigfile = 'rtmpconfig.ini'
-config = SafeConfigParser()
-config.read(rtmpconfigfile)
-hostName = config.get('scriptconfig', 'bind_to_ip')
-hostPort = config.getint('scriptconfig', 'bind_to_port')
-logfile = config.get('scriptconfig', 'log_to_file')
-logging.basicConfig(filename=logfile, level=logging.INFO)
-sys.path.append('../')
+configfile = 'config.ini'
+config = ConfigParser()
+config.read(configfile)
+hostName = config.get('main', 'bind_to_ip')
+hostPort = config.getint('main', 'bind_to_port')
+authfile = config.get('main', 'authentication_file')
+logfile = config.get('main', 'log_to_file')
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler(logfile), logging.StreamHandler()])
 
-class MyServer(BaseHTTPRequestHandler):
-    def _auth_success(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-    def _auth_fail(self):
-        self.send_response(404)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<h1>why hello there general kenobi.</h1>", "utf-8"))
-        self.wfile.write(bytes("<h1>------------------</h1>", "utf-8"))
-        self.wfile.write(bytes("<h1>you seem to have taken a wrong turn young jedi. you must leave before the the sith finds you.</h1>", "utf-8"))
-        print('--------------------')
-        ipaddress = self.client_address[0]
-        print('GET request recieved from IP: ' + str(ipaddress))
-        logging.info('--------------------')
-        logging.info('GET request recieved from IP: ' + str(ipaddress))
-    def do_POST(self):
-        print('--------------------')
-        logging.info('--------------------')
-        ipaddress = self.client_address[0]
-        print('Recieved new auth request from IP: ' + str(ipaddress))
-        logging.info('Recieved new auth request from IP: ' + str(ipaddress))
-        config.read(rtmpconfigfile)
-        sections = config.sections()
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        data = post_data.decode('utf-8')
-        params = dict(x.split('=') for x in data.split('&'))
-        application = params['app']
-        key = params['name']
-        if application == 'scriptconfig': # Ignore script config calls
-            logging.warning('User tried to call script config')
-            return
-        if application in sections:
-            keys = json.loads(config.get(application, 'streamkeys'))
-            if key in keys:
-                logging.info('Connection authenticated on application: ' + str(application) + ' and stream key: ' + str(key))
-                print('Connection authenticated on application: ' + str(application) + ' and stream key: ' + str(key))
-                self._auth_success()
-            else:
-                logging.warning('User failed to authenticate.')
-                print('User failed to authenticate.')
-                self._auth_fail()
+def read_auth():
+    with open(authfile) as json_file:
+        try:
+            data = json.load(json_file)
+        except json.JSONDecodeError:
+            logging.error('Could not decode JSON authentication file! Exiting...')
+            sys.exit()
+        return data
 
-myServer = HTTPServer((hostName, hostPort), MyServer)
-logging.info("Server Starts - %s:%s" % (hostName, hostPort))
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+async def authhandle(request):
+    ipaddress = request.remote
+    logging.info('--------------------')
+    logging.info('Recieved new auth request from IP: ' + str(ipaddress))
+    authtable = read_auth()
+    body = await request.post()
+    if 'app' in body.keys() and body['app'] in authtable.keys():
+        if body['name'] in authtable[body['app']]:
+            logging.info('Publish authenticated. (App: {}, Streamname {})'.format(body['app'], body['name']))
+            return web.Response(status=200)
+        else:
+            logging.warning('Publish failed to authenticate. (App: {}, Streamname {})'.format(body['app'], body['name']))
+            return web.Response(status=401)
+    return web.Response(status=404)
 
-try:
-    myServer.serve_forever()
-
-except KeyboardInterrupt:
-    pass
-
-myServer.server_close()
-print('--------------------')
-logging.info('--------------------')
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
-logging.info("Server Stops - %s:%s" % (hostName, hostPort))
+app.add_routes([web.post('/auth/', authhandle)])
+web.run_app(app, host=hostName, port=hostPort)
